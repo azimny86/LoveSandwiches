ebc8b2fd0048b57cbc4428a02eb2ebbbd658f461
azimny86 <azimny86@gmail.com>
2022-12-28 12:14:36 +0000

    Calculete surplus data

--- run.py
+++ run.py
@@ -77,8 +77,13 @@ def calculate_surplus_date(sales_row):
     print("Calculating surplus data ...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    pprint(stock_row)
 
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
 
 def main():
     """
@@ -87,7 +92,9 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     updata_saleas_worksheet(sales_data)
-    calculate_surplus_date(sales_data)
+    new_surplus_data = calculate_surplus_date(sales_data)
+    print(new_surplus_data)
+
 
 print("Welcom to Love Sandwitches data automation")
 main()
